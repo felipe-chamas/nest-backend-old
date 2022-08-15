ee32846dc436df234babde2ea04001d7a6211dfa
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2022-08-14 21:06:53 -0300

    Remove autogenerated file

--- packages/smart-contracts/tasks/deploy/09_nft_launchpad.ts
+++ packages/smart-contracts/tasks/deploy/09_nft_launchpad.ts
@@ -9,9 +9,9 @@ task(TASK_DEPLOY_NFT_LAUNCHPAD, 'Deploy NFT Launchpad contract')
   .addParam('symbol', 'Token symbol', undefined, types.string)
   .addParam('launchpad', 'Launchpad contract address', undefined, types.string)
   .addParam('maxTokenSupply', 'Maximum token supply', undefined, types.string)
+  .addParam('burnEnabled', 'Enable Token burning (Needs to be "true" for NFT Boxes)', undefined, types.boolean)
   .addOptionalParam('baseUri', 'Base Token URI', '', types.string)
   .addOptionalParam('silent', 'Silent', false, types.boolean)
-  .addOptionalParam('burnEnabled', 'Enable Token burning (Needs to be "true" for NFT Boxes)', false, types.boolean)
   .setAction(
     async (
       { acl, name, symbol, launchpad, baseUri, maxTokenSupply, silent, burnEnabled }: NFTLaunchpadConstructor,
