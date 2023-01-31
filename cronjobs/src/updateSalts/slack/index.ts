a5c86f5324186d91badb51da16baa95cc31decbf
nicodeheza <nicolasdeheza@hotmail.com>
2023-01-31 09:20:07 -0300

    cronjob: don't alert on local

--- cronjobs/src/updateSalts/slack/index.ts
+++ cronjobs/src/updateSalts/slack/index.ts
@@ -4,15 +4,19 @@ import getEnv from '../constants/env'
 
 export default async function slackAlert(message: string) {
   const slackUrl = getEnv('SLACK_URL')
-  await axios.post(slackUrl, {
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `*Update pin container*: \n - *message:* ${message}`
+  const stage = getEnv('STAGE')
+
+  if (stage !== 'local') {
+    await axios.post(slackUrl, {
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*Update pin container*: \n - *message:* ${message}`
+          }
         }
-      }
-    ]
-  })
+      ]
+    })
+  }
 }
